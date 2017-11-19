3ce3b6162b08908c778433ed55564ab85cbc46fa
Benjamin Sisson <benmsisson@gmail.com>
2017-11-19 14:19:23 -0500

    Debugging 15

--- responder.js
+++ responder.js
@@ -23,7 +23,9 @@ function handleLatLng(uuid,latLng,callback) {
         function (err, response) {
             console.log("Got back to me");
             console.log(response.json.results);
-            console.log(Object.keys(response.json.results).length === 0 && response.json.results.constructor === Object);
+            console.log(Object.keys(response.json.results));
+            console.log(Object.keys(response.json.results).length);
+            console.log(Object.keys(response.json.results).length === 0);
             if (!response.json.results) {
                 console.log("Nothing my guy");
             }
