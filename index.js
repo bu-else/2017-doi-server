6daee137e9581f261ef6225ac472e50c24fe26fc
Benjamin Sisson <benmsisson@gmail.com>
2017-11-30 14:34:16 -0500

    Testing shorter duration

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const stageAddress = 2;
 
 // If expirationTime is set to -1, requests will never expire
 // Otherwise, a good value is ten minutes
-const tenMinutes = 10 * 60 * 1000
+const tenMinutes = 30 * 1000;//10 * 60 * 1000
 const expirationTime = tenMinutes;
 
 const requestHandler = (request, response) => {
